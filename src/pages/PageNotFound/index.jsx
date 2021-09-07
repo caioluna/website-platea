fa1f7c5641205ba7ac3fe13d65252a7f2122878c
Caio Luna <ccluna@gmail.com>
2021-09-07 20:04:57 -0300

    update:404 page-not-found layout

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,9 +1,131 @@
-import React from 'react';
+import { motion } from 'framer-motion'
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import GeoForm from '../../components/GeoForm'
+import CloseButton from '../../components/CloseButton'
+
+const container = {
+	animate: {
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.1,
+		},
+	},
+}
+
+const ErrorVariants = {
+	initial: {
+		opacity: 0,
+		scale: 0,
+	},
+	animate: {
+		opacity: 1,
+		scale: 1,
+		transition: {
+			type: 'spring',
+			duration: 0.5,
+		},
+	},
+}
+
+const titleVariants = {
+	initial: { opacity: 0, y: -300 },
+	animate: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			type: 'spring',
+			duration: 0.6,
+		},
+	},
+}
+
+const textVariants = {
+	initial: { opacity: 0, x: 300 },
+	animate: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			type: 'spring',
+			duration: 1,
+		},
+	},
+}
 
 export default function PageNotFound() {
 	return (
-		<div>
-			<h1>page not found</h1>
-		</div>
-	);
+		<Container
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, transition: { ease: 'easeInOut', duration: 0.5 } }}
+			exit={{ opacity: 0 }}
+		>
+			<CloseButton />
+			<GeoForm />
+
+			<Content variants={container} initial='initial' animate='animate'>
+				<motion.span variants={ErrorVariants}>404</motion.span>
+				<motion.h1 variants={titleVariants}>Eita!</motion.h1>
+				<motion.p variants={textVariants}>
+					Não encontramos essa página...
+				</motion.p>
+				<motion.p variants={textVariants}>
+					Que tal voltar para a <Link to='/'>página inicial</Link> e tentar de
+					novo?
+				</motion.p>
+			</Content>
+		</Container>
+	)
 }
+
+const Container = styled(motion.main)`
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	align-items: flex-start;
+	justify-content: center;
+
+	color: #fff;
+`
+
+const Content = styled(motion.section)`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: flex-start;
+
+	height: 100vh;
+	width: 1040px;
+
+	span {
+		position: absolute;
+		right: 80px;
+		top: 30px;
+
+		font-size: 200px;
+		font-family: 'DINAlternateBlack';
+		color: #fddb00;
+	}
+
+	h1 {
+		font-size: 64px;
+		line-height: 70px;
+		text-align: center;
+		font-family: 'DINAlternateBlack', sans-serif;
+		color: #fddb00;
+		margin-bottom: 30px;
+	}
+
+	a {
+		color: #fddb00;
+	}
+
+	p {
+		font-size: 16px;
+		line-height: 30px;
+		color: #fff;
+	}
+`
